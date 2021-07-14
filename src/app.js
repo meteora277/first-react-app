f797498b921af06bb6f77d9871aec57bc31ecf72
Justin Diaz <justin.s.diaz@gmail.com>
2021-07-14 00:04:33 -0400

    added

---
+++ src/app.js
@@ -0,0 +1,19 @@
+import React from "react"
+import List from './list'
+
+
+function App() {
+
+    return (
+        <div>
+           
+            <List/>
+        </div>
+       
+    )
+
+  
+
+}
+
+export default App
---
+++ src/list.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+function List(){
+
+    return(
+            <ul>
+                <li>potato</li>
+                <li>ice cream</li>
+                <li>lettuce</li>
+            </ul>
+    )
+
+}
+
+export default List
